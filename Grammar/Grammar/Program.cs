3e158851ea3a5bee54bc94ca19fdf960277c6b9d
wakinglife000 <wakinglife000@gmail.com>
2017-07-30 12:03:32 +0900

    if ... else -> switch 변경 퀘스트 연습

--- Grammar/Grammar/Program.cs
+++ Grammar/Grammar/Program.cs
@@ -10,15 +10,23 @@ namespace Grammar
     {
         static void Main(string[] args)
         {
-            // do...while 반복문
+            // do...while 반복문: while 조건이 참일 때만 do 내용 반복 실행
             string snum;
             int num;
             do
             {
                 Console.Write("숫자를 입력하세요(끝낼 때는 0) ");
                 snum = Console.ReadLine();
-                num = Convert.ToInt32(snum);
 
+                //공백 예외처리
+                if (string.IsNullOrEmpty(snum))
+                {
+                    Console.WriteLine("입력값이 없어 프로그램을 종료합니다.");
+                    break;
+                }
+
+                num = Convert.ToInt32(snum);
+             
                 // quest1 : for -> foreach
                 // quest2 : if ... else -> switch
                 // quest3 : do ... while -> while
@@ -39,8 +47,25 @@ namespace Grammar
                 {
                     Console.WriteLine("1~{0}까지의 합계는 {1}입니다.", num, sum);
                 }
+                
 
-            } while (num != 0);
+                /*quest2
+                switch (num)
+                {
+                    case 0:
+                        Console.WriteLine("프로그램을 종료합니다.");
+                        break;
+
+                    default:
+                        Console.WriteLine("1~{0}까지의 합계는 {1}입니다.", num, sum);
+                        break;
+                }
+                */
+
+            /**/
+
+
+            } while (num != 0); //0이 아니면(true) 계속 실행/ 0일 때(false) 중지
         }
     }
 }
